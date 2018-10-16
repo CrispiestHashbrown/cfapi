45645cb36e7bb52553daefd15b0940d06256700e
RK <r_kieu@outlook.com>
2018-10-15 22:15:17 -0700

    Created GitHub auth token validator
    This token validator calls the GitHub API to check an auth
    token's scopes. This is necessary since the user can alter
    token scopes after the auth flow.
    
    See: https://developer.github.com/v3/guides/basics-of-authentication/

---
+++ helpers/TokenValidator.js
@@ -0,0 +1,35 @@
+const request = require('request');
+const config = require('config');
+const _ = require('lodash');
+const client_id = config.get('appauth.client_id');
+
+function isAccessTokenValid (token, callback) {
+  const url = `https://api.github.com/applications/${client_id}/tokens/${token}`;
+  request.get(url, {
+    'auth': {
+      'user': client_id,
+      'pass': config.get('appauth.client_secret')
+    },
+    headers: {
+      'User-Agent': 'CrispiestHashbrown',
+      Date: new Date(),
+      'Accept': 'application/json'
+    }
+  }, function (err, res, body) {
+    if (!err && res.statusCode === 200) {
+      const scopeObject = [
+        'public_repo',
+        'read:repo_hook',
+        'read:user',
+        'user:follow'
+      ];
+      const parsedBody = JSON.parse(body);
+      const validationResult = _.isEqual(parsedBody.scopes, scopeObject);
+      callback(validationResult);
+    } else {
+      console.log('There was an error while accessing the Github API.', err);
+    }
+  });
+}
+
+module.exports = isAccessTokenValid;
