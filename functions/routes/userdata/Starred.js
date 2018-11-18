62ece3c7f3a1b92820057f1f62c69d283c7fd344
RK <r_kieu@outlook.com>
2018-11-17 22:51:46 -0800

    Added all routes for Starred
    Route for getting repos starred by the user
    Route for checking if a repo is starred by the user
    Route for starring a repo
    Route for unstarring a repo

---
+++ functions/routes/userdata/Starred.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const query = require('../../helpers/QueryParser');
+
+router.use(express.json());
+
+// -- routes --
+// GET starred repos
+router.get('/', (req, res) => {
+  const access_token = req.session.access_token;
+  if (!access_token) {
+    return res.status(401).send('Unauthorized request');
+  }
+
+  const url = `https://api.github.com/user/starred${query(req.url)}`;
+  request.get(url, {
+    headers: {
+      'Authorization': `bearer ${access_token}`,
+      'User-Agent': 'CrispiestHashbrown',
+      'Accept': 'application/json'
+    }
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const starredRepos = JSON.parse(body);
+      return res.status(response.statusCode).send(starredRepos);
+    } else {
+      console.log(`${response.statusCode} response: Error accessing the Github API.`, error);
+    }
+  });
+});
+
+// GET to check if user is starring a repository
+router.get('/:owner/:repo', (req, res) => {
+  const access_token = req.session.access_token;
+  if (!access_token) {
+    return res.status(401).send('Unauthorized request');
+  }
+
+  const url = `https://api.github.com/user/starred/${req.params.owner}/${req.params.repo}`;
+  request.get(url, {
+    headers: {
+      'Authorization': `bearer ${access_token}`,
+      'User-Agent': 'CrispiestHashbrown',
+      'Accept': 'application/json'
+    }
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 204) {
+      return res.status(response.statusCode).send('No Content');
+    } else {
+      console.log(`${response.statusCode} response: Error accessing the Github API.`, error);
+    }
+  });
+});
+
+// PUT to star a repo
+router.put('/:owner/:repo', (req, res) => {
+  const access_token = req.session.access_token;
+  if (!access_token) {
+    return res.status(401).send('Unauthorized request');
+  }
+
+  const url = `https://api.github.com/user/starred/${req.params.owner}/${req.params.repo}`;
+  request.put(url, {
+    headers: {
+      'Authorization': `bearer ${access_token}`,
+      'User-Agent': 'CrispiestHashbrown',
+      'Accept': 'application/json',
+      'Content-Length': 0
+    }
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 204) {
+      return res.status(response.statusCode).send('No Content');
+    } else {
+      console.log(`${response.statusCode} response: Error accessing the Github API.`, error);
+    }
+  });
+});
+
+// DELETE to unstar repo
+router.delete('/:owner/:repo', (req, res) => {
+  const access_token = req.session.access_token;
+  if (!access_token) {
+    return res.status(401).send('Unauthorized request');
+  }
+
+  const url = `https://api.github.com/user/starred/${req.params.owner}/${req.params.repo}`;
+  request.delete(url, {
+    headers: {
+      'Authorization': `bearer ${access_token}`,
+      'User-Agent': 'CrispiestHashbrown',
+      'Accept': 'application/json'
+    }
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 204) {
+      return res.status(response.statusCode).send('No Content');
+    } else {
+      console.log(`${response.statusCode} response: Error accessing the Github API.`, error);
+    }
+  });
+});
+
+module.exports = router;
