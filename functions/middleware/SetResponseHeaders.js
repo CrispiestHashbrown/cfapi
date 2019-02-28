3deec994f5a777fd08f3b6b38f711b701c74f829
RK <r_kieu@outlook.com>
2019-02-27 21:12:43 -0800

    Added options preflight to CORS options and response headers

--- functions/middleware/SetResponseHeaders.js
+++ functions/middleware/SetResponseHeaders.js
@@ -3,9 +3,12 @@ const firebase = require('firebase-admin');
 function setResponseHeaders (req, res, next) {
   res.setHeader('Date', firebase.firestore.Timestamp.now().toDate());
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.set('Access-Control-Expose-Headers', 'Link');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({});
+  }
   next();
 }
 
